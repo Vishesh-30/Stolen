65120d857e8e1f9865d93b9b813734c330d3ff21
Vishesh Phutela <visheshphutela30@gmail.com>
2024-06-04 12:27:51 +0530

    Initial Setup

---
+++ frontend/src/components/LandingPage.vue
@@ -0,0 +1,12 @@
+<template>
+    <div class="home">
+        <h1>Home</h1>
+        <p>Welcome to Stolen Project</p>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'LandingPage',
+}
+</script>
